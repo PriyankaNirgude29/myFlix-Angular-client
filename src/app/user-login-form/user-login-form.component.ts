9748c267cb9fa0487a3e1f2612c4907d10e57ee6
PriyankaNirgude29 <piu.pawar@gmail.com>
2022-07-13 18:49:57 +0200

    Update user-login-form.component.ts

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-login-form',
@@ -16,7 +17,9 @@ export class UserLoginFormComponent implements OnInit {
 
   constructor( public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar) { }
+    public snackBar: MatSnackBar,
+    public router: Router
+    ) { }
 
   ngOnInit(): void {
   }
@@ -28,9 +31,7 @@ export class UserLoginFormComponent implements OnInit {
         console.log(r);
         localStorage.setItem('token', r.token);
       localStorage.setItem('user', r.user.Username);
-        this.snackBar.open(r,'OK',{
-          duration: 2000
-        });
+      this.router.navigate(['movies']);
     } , 
     error: (e) => {
       console.log(e);
